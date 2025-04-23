735b73c10ec6659aec66eb22fb0b79e9d7e63251
Debasish <debasish.dhal117@gmail.com>
2025-04-23 05:11:53 +0000

    code refactor and handle when delimeter is changed

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,15 @@ def add(numbers)
     return 0 if numbers.empty?
     # numbers.split(',').map(&:to_i).reduce(0, :+)
     # numbers.split(/[\s,]+/).map(&:to_i).reduce(0, :+)
-    nums = numbers.split(/[\s,]+/).map(&:to_i)
+    delimiter = /[\s,]+/
+
+    if numbers.start_with?("//")
+        parts = numbers.split("\n", 2)
+        delimiter = Regexp.escape(parts[0][2..])
+        numbers = parts[1]
+    end
+
+    nums = numbers.split(/#{delim}/).map(&:to_i)
     negatives = nums.select { |num| num < 0 }
     raise ArgumentError, "negative numbers are not allowed: #{negatives.join(', ')}" unless negatives.empty?
     nums.reduce(0, :+)
