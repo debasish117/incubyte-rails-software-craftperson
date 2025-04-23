fe6438267e7824e2bd2edea5483496dddbb19c97
Debasish <debasish.dhal117@gmail.com>
2025-04-23 04:38:33 +0000

    handle when i/p is single and multiple string numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,4 @@
 def add(numbers)
     return 0 if numbers.empty?
+    numbers.split(',').map(&:to_i).reduce(0, :+)
 end
--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -6,4 +6,14 @@ class StringCalculatorTest < Minitest::Test
     def test_add_empty_string
         assert_equal 0, add("")
     end
+
+    def test_add_single_number
+        assert_equal 1, add("1")
+        assert_equal 5, add("5")
+    end
+
+    def test_add_two_numbers
+        assert_equal 3, add("1,2")
+        assert_equal 7, add("3,4")
+    end
 end
